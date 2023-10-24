2a06d4425eb8f6f89981dd67e66797933b0146ec
Anas KBL <kabila.anas1@gmail.com>
2023-10-24 13:02:56 +0100

    #3 added scroll view

--- lib/pages/signin_page.dart
+++ lib/pages/signin_page.dart
@@ -19,104 +19,106 @@ class _SigninPageState extends State<SigninPage> {
       resizeToAvoidBottomInset: false,
       body: Container(
         margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.10),
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: MediaQuery.of(context).size.height * 0.06),
-        child: Column(
-          children: [
-            //AppText aligned to the left
-            Align(
-              alignment: Alignment.centerLeft,
-              child: AppText(
-                text: 'Hey again!',
-                fontSize: 34,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: MediaQuery.of(context).size.height * 0.05),
-            AppInput(
-              icon: Icons.email,
-              hintText: 'Email',
-            ),
-            SizedBox(height: 20),
-            AppInput(
-              icon: Icons.lock,
-              hintText: 'Password',
-              obscureText: true,
-            ),
-            SizedBox(height: 20),
-            AppButton(
-              text: 'Sign in',
-              fontSize: 20,
-              borderRadius: 75,
-            ),
-            SizedBox(height: 10),
-            Align(
-              alignment: Alignment.center,
-              child: AppText(
-                text: 'Forgot password?',
-                fontSize: 16,
-                fontWeight: FontWeight.normal,
-                color: AppColors.primaryColor,
-              ),
-            ),
-            SizedBox(height: 20),
-            //a horizontal line with text in the middle
-            Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    height: 1,
-                    color: AppColors.textColor,
-                  ),
-                ),
-                SizedBox(width: 10),
-                AppText(
-                  text: 'Or continue with',
-                  fontSize: 16,
+        child: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: MediaQuery.of(context).size.height * 0.06),
+          child: Column(
+            children: [
+              //AppText aligned to the left
+              Align(
+                alignment: Alignment.centerLeft,
+                child: AppText(
+                  text: 'Hey again!',
+                  fontSize: 34,
                   fontWeight: FontWeight.bold,
-                  color: AppColors.secondaryColor,
                 ),
-                SizedBox(width: 10),
-                Expanded(
-                  child: Container(
-                    height: 1,
-                    color: AppColors.textColor,
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+              AppInput(
+                icon: Icons.email,
+                hintText: 'Email',
+              ),
+              SizedBox(height: 20),
+              AppInput(
+                icon: Icons.lock,
+                hintText: 'Password',
+                obscureText: true,
+              ),
+              SizedBox(height: 20),
+              AppButton(
+                text: 'Sign in',
+                fontSize: 20,
+                borderRadius: 75,
+              ),
+              SizedBox(height: 10),
+              Align(
+                alignment: Alignment.center,
+                child: AppText(
+                  text: 'Forgot password?',
+                  fontSize: 16,
+                  fontWeight: FontWeight.normal,
+                  color: AppColors.primaryColor,
+                ),
+              ),
+              SizedBox(height: 20),
+              //a horizontal line with text in the middle
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 1,
+                      color: AppColors.textColor,
+                    ),
                   ),
+                  SizedBox(width: 10),
+                  AppText(
+                    text: 'Or continue with',
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: AppColors.secondaryColor,
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: Container(
+                      height: 1,
+                      color: AppColors.textColor,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              //an empty container with a border
+              Container(
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  border: Border.all(color: AppColors.textColor),
+                  borderRadius: BorderRadius.circular(10),
                 ),
-              ],
-            ),
-            SizedBox(height: 20),
-            //an empty container with a border
-            Container(
-              height: 50,
-              width: 50,
-              decoration: BoxDecoration(
-                border: Border.all(color: AppColors.textColor),
-                borderRadius: BorderRadius.circular(10),
+                //google logo and onpressed function
+                child: IconButton(
+                  icon: Image.asset(AppIcons.icGoogle),
+                  onPressed: () {},
+                ),
               ),
-              //google logo and onpressed function
-              child: IconButton(
-                icon: Image.asset(AppIcons.icGoogle),
-                onPressed: () {},
+              SizedBox(height: 20),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: AppText(
+                  text: 'New here?',
+                  fontSize: 34,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            SizedBox(height: 20),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: AppText(
-                text: 'New here?',
-                fontSize: 34,
-                fontWeight: FontWeight.bold,
+              SizedBox(height: 5),
+              AppButton(
+                text: 'Sign up',
+                fontSize: 20,
+                borderRadius: 75,
+                backgroundColor: AppColors.thirdColor,
               ),
-            ),
-            SizedBox(height: 5),
-            AppButton(
-              text: 'Sign up',
-              fontSize: 20,
-              borderRadius: 75,
-              backgroundColor: AppColors.thirdColor,
-            ),
-          ],
-        ),
+            ],
+          ),
+        )
       ),
     );
   }
