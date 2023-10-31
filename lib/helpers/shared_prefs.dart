1439cb6efe570d325970c33d62f7072b34409f46
Anas KBL <kabila.anas1@gmail.com>
2023-10-31 12:06:03 +0100

    #6 added MapBox api with userlocation tracking

---
+++ lib/helpers/shared_prefs.dart
@@ -0,0 +1,5 @@
+import 'package:latlong2/latlong.dart';
+
+LatLng getCurrentLatLngFromSharedPrefs() {
+  return const LatLng(59.5090214, -0.1982948);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:taxi_fleet_frontend_app/pages/signin_page.dart';
+import 'package:taxi_fleet_frontend_app/pages/main_page.dart';
+//import 'package:taxi_fleet_frontend_app/pages/signin_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SigninPage(),
+      home: const MainPage(),
     );
   }
 }
