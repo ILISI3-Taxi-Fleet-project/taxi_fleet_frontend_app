d4dc0c4a47bf1e596a46ec15564beb497e335804
Anas KBL <kabila.anas1@gmail.com>
2023-11-24 19:53:18 +0100

    #27 fixed signup page scroll

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:taxi_fleet_frontend_app/pages/client/main_page.dart' as clientHomePage;
 import 'package:taxi_fleet_frontend_app/pages/driver/main_page.dart' as driverHomePage;
 import 'package:taxi_fleet_frontend_app/pages/signin_page.dart';
+import 'package:taxi_fleet_frontend_app/pages/signup_page.dart';
 import 'package:taxi_fleet_frontend_app/providers/location_provider.dart';
 
 void main() {
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SigninPage(),
+      home: const SignUpPage(),
       routes:{
         '/passengerHome': (context) => const clientHomePage.MainPage(),
         '/driverHome': (context) => const driverHomePage.MainPage(),
--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -40,148 +40,152 @@ class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,     
+      resizeToAvoidBottomInset: true,     
       appBar: AppBar(
-        title: Text("Fill your profile"),
+        title: const Text("Fill your profile"),
         // add margin to the top of the app bar
         toolbarHeight: 80,
       ),
-      body: Container(
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
         child: SingleChildScrollView(
-          padding: EdgeInsets.symmetric(horizontal: 30, vertical: MediaQuery.of(context).size.height * 0.04),
-          child:Column(
-            children: [
-              // picture chooser, as a cercle  with a plus sign
-              Center(
-                child: Container(
-                  width: 100, // Adjust the size as needed
-                  height: 100, // Adjust the size as needed
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: AppColors.secondaryColor, // Background color for unknown picture
-                  ),
-                  child: Stack(
-                    alignment: Alignment.bottomRight,
-                    children: [
-                      Container(
-                        width: 30,
-                        height: 30,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: AppColors.primaryColor
-                        ),
-                        child:Align(
-                            alignment: Alignment.center,
-                            child:Icon(
-                              Icons.add, // Plus sign icon
-                              size: 20, // Adjust the size as needed
-                              color: Colors.white,
-                            )
-                        ),
-                        
-                      )
-                    ],
+        //keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+        padding: EdgeInsets.symmetric(horizontal: 30, vertical: MediaQuery.of(context).size.height * 0.04),
+        child:Column(
+          children: [
+            // picture chooser, as a cercle  with a plus sign
+            Center(
+              child: Container(
+                width: 100, // Adjust the size as needed
+                height: 100, // Adjust the size as needed
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: AppColors.secondaryColor, // Background color for unknown picture
+                ),
+                child: Stack(
+                  alignment: Alignment.bottomRight,
+                  children: [
+                    Container(
+                      width: 30,
+                      height: 30,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: AppColors.primaryColor
+                      ),
+                      child:const Align(
+                          alignment: Alignment.center,
+                          child:Icon(
+                            Icons.add, // Plus sign icon
+                            size: 20, // Adjust the size as needed
+                            color: Colors.white,
+                          )
+                      ),
+                      
+                    )
+                  ],
+                )
+              ),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.04),
+            AppInput(
+              icon: Icons.person,
+              hintText: 'First name',
+              controller: firstNameController,
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+            AppInput(
+              icon: Icons.calendar_today,
+              hintText: 'Last name',
+              controller: lastNameController,
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+            AppInput(
+              icon: Icons.phone,
+              hintText: 'Phone number',
+              controller: phoneNumberController,
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+            // Client Type
+            Row(
+              children: [
+                Expanded(
+                  child:SizedBox(
+                    height: 60,
+                    child: ElevatedButton(
+                      onPressed: () => {
+                        setState(() {
+                          _clientType = clientType.driver;
+                        })
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: _clientType == clientType.driver ? AppColors.primaryColor : Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                        )
+                      ),
+                      child: Text("Driver", style: TextStyle(color: _clientType == clientType.driver ? Colors.white : AppColors.textColor),),
+                    ),
                   )
                 ),
-              ),
-              SizedBox(height: MediaQuery.of(context).size.height * 0.04),
-              AppInput(
-                icon: Icons.person,
-                hintText: 'First name',
-                controller: firstNameController,
-              ),
-              SizedBox(height: MediaQuery.of(context).size.height * 0.03),
-              AppInput(
-                icon: Icons.calendar_today,
-                hintText: 'Last name',
-                controller: lastNameController,
-              ),
-              SizedBox(height: MediaQuery.of(context).size.height * 0.03),
-              AppInput(
-                icon: Icons.phone,
-                hintText: 'Phone number',
-                controller: phoneNumberController,
-              ),
-              SizedBox(height: MediaQuery.of(context).size.height * 0.03),
-              // Client Type
-              Row(
-                children: [
-                  Expanded(
-                    child:SizedBox(
-                      height: 60,
-                      child: ElevatedButton(
-                        onPressed: () => {
-                          setState(() {
-                            _clientType = clientType.driver;
-                          })
-                        },
-                        child: Text("Driver", style: TextStyle(color: _clientType == clientType.driver ? Colors.white : AppColors.textColor),),
-                        style: ElevatedButton.styleFrom(
-                          primary: _clientType == clientType.driver ? AppColors.primaryColor : Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10)
-                          )
-                        ),
+                const SizedBox(width: 20),
+                Expanded(
+                  child:SizedBox(
+                    height: 60,
+                    child: ElevatedButton(
+                      onPressed: () => {
+                        setState(() {
+                          _clientType = clientType.passenger;
+                        })
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: _clientType == clientType.passenger ? AppColors.primaryColor : Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                        )
                       ),
-                    )
-                  ),
-                  SizedBox(width: 20),
-                  Expanded(
-                    child:SizedBox(
-                      height: 60,
-                      child: ElevatedButton(
-                        onPressed: () => {
-                          setState(() {
-                            _clientType = clientType.passenger;
-                          })
-                        },
-                        child: Text("Passenger", style: TextStyle(color: _clientType == clientType.passenger ? Colors.white : AppColors.textColor),),
-                        style: ElevatedButton.styleFrom(
-                          primary: _clientType == clientType.passenger ? AppColors.primaryColor : Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10)
-                          )
-                        ),
-                      ),
-                    )
-                  ),
-
-                ],
-              ),
-              // email
-              SizedBox(height: MediaQuery.of(context).size.height * 0.03),
-              AppInput(
-                icon: Icons.email,
-                hintText: 'Email',
-                controller: emailController,
-              ),
-              // password
-              SizedBox(height: MediaQuery.of(context).size.height * 0.03),
-              AppInput(
-                icon: Icons.lock,
-                hintText: 'Password',
-                controller: passwordController,
-                obscureText: _isObscure,
-                suffixIcon: IconButton(
-                  icon: Icon(
-                    _isObscure ? Icons.visibility_off : Icons.visibility,
-                    color: AppColors.secondaryColor,
-                  ),
-                  onPressed: () => {
-                    setState(() {
-                      _isObscure = !_isObscure;
-                    })
-                  },
+                      child: Text("Passenger", style: TextStyle(color: _clientType == clientType.passenger ? Colors.white : AppColors.textColor),),
+                    ),
+                  )
                 ),
+
+              ],
+            ),
+            // email
+            SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+            AppInput(
+              icon: Icons.email,
+              hintText: 'Email',
+              controller: emailController,
+            ),
+            // password
+            SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+            AppInput(
+              icon: Icons.lock,
+              hintText: 'Password',
+              controller: passwordController,
+              obscureText: _isObscure,
+              suffixIcon: IconButton(
+                icon: Icon(
+                  _isObscure ? Icons.visibility_off : Icons.visibility,
+                  color: AppColors.secondaryColor,
+                ),
+                onPressed: () => {
+                  setState(() {
+                    _isObscure = !_isObscure;
+                  })
+                },
               ),
-              SizedBox(height: MediaQuery.of(context).size.height * 0.03),
-            ],
-          )
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+          ],
         )
+      ),
       ),
       //footer submit button
       bottomNavigationBar: Container(
-        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
         child: AppButton(
           text: 'Submit',
           fontSize: 20,
